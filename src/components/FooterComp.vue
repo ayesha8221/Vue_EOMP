274f73212fff852745074dc758cf0dafa2ac73f8
ayesha <ayesha.glnt012@gmail.com>
2023-11-06 16:25:27 +0200

    links

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -7,12 +7,12 @@
             </a>
             </div>
             <div class="icon">
-                <a href="www.linkedin.com/in/ayesha-galant-329780284" target="_blank">
+                <a href="https://www.linkedin.com/in/ayesha-galant-329780284/" target="_blank">
                 <img src="https://i.postimg.cc/MGnmcd7K/img-icons8.png" alt="linkedin">
             </a>
             </div>
             <div class="icon">
-                <a href="https://accounts.google.com/v3/signin/identifier?authuser=0&continue=https%3A%2F%2Fwww.google.com%2Fsearch%3Fq%3Dgmail%2Bloin%26oq%3Dgmail%2Bloin%26gs_lcrp%3DEgZjaHJvbWUyBggAEEUYOTIPCAEQABgKGIMBGLEDGIAEMg8IAhAAGAoYgwEYsQMYgAQyCQgDEAAYChiABDIPCAQQABgKGIMBGLEDGIAEMgwIBRAAGAoYsQMYgAQyDwgGEAAYChiDARixAxiABDIPCAcQABgKGIMBGLEDGIAEMgkICBAAGAoYgAQyDwgJEAAYChiDARixAxiABNIBCDc5MzRqMGo0qAIAsAIA%26sourceid%3Dchrome%26ie%3DUTF-8&ec=GAlAAQ&hl=en&flowName=GlifWebSignIn&flowEntry=AddSession&dsh=S-1324816552%3A1697792959483621&theme=glif" target="_blank">
+                <a href="mailto:ayesha.glnt012@gmail.com" target="_blank">
                 <img src="https://i.postimg.cc/pL4nSxjR/img-icons8.png" alt="gmail">
             </a>
             </div>
